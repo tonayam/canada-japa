3283ca9a812c681b236f3a0f29f9cb777dc88a82
tonayam <tonayam99@gmail.com>
2022-08-08 03:57:23 +0100

    Landing Page

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,7 +5,6 @@ const menuBtn = document.querySelector(`.hamburger`);
 const navItems = document.querySelector(`.nav-items`);
 
 hamburgerBtn.addEventListener(`click`, () => {
-  console.log(`clicked`);
   hamburgerBtn.classList.toggle(`open`);
   navItems.classList.toggle(`open`);
 });
